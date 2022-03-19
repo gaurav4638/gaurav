f60f0459dbfc12abaa46e49e06ed486424766130
gaurav4638 <101918512+gaurav4638@users.noreply.github.com>
2022-03-19 11:54:31 +0530

    Add files via upload

---
+++ gaurav.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{ 
+   printf("*\n\t\t");
+   printf("**\n\t\t");
+   printf("*\n\t");
+  return 0;
+}
---
+++ string1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ int str_length(char[])
+ int str_compare(char[],char[])
+ void str_concat(char[],char[])
+ int main()
+{ 
+  char str[50];
+  char str1[50],
